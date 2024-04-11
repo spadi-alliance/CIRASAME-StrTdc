75ef34d7d4249fe8bafce7cad2610591d4bc160c
Ryotaro Honda <honda@lambda.phys.tohoku.ac.jp>
2024-04-11 17:22:36 +0900

    v2.2

--- project.tcl
+++ project.tcl
@@ -207,8 +207,7 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/hdl/mikumari/cbt/CdcmRx.vhd"]"\
  "[file normalize "$origin_dir/hdl/mikumari/cbt/CbtRx.vhd"]"\
  "[file normalize "$origin_dir/hdl/mikumari/cbt/CbtLane.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/main/DCR_NetAssign.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/main/DelayGen.vhd"]"\
+  "[file normalize "$origin_dir/hdl/common/main/DelayGen.vhd"]"\
  "[file normalize "$origin_dir/hdl/strtdc/defDataBusAbst.vhd"]"\
  "[file normalize "$origin_dir/hdl/laccp/laccp/defLaccp.vhd"]"\
  "[file normalize "$origin_dir/hdl/laccp/laccp/defHeartBeatUnit.vhd"]"\
@@ -243,8 +242,7 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/hdl/laccp/laccp/RLIGP.vhd"]"\
  "[file normalize "$origin_dir/hdl/laccp/laccp/RCAP.vhd"]"\
  "[file normalize "$origin_dir/hdl/laccp/laccp/LaccpMainBlock.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/sitcp/defMiiRstTimer.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/sitcp/MiiRstTimer.vhd"]"\
+ "[file normalize "$origin_dir/hdl/common/main/RstDelayTimer.vhd"]"\
  "[file normalize "$origin_dir/hdl/mikumari/mikumari-link/PRBS16.vhd"]"\
  "[file normalize "$origin_dir/hdl/mikumari/mikumari-link/MikumariTx.vhd"]"\
  "[file normalize "$origin_dir/hdl/mikumari/mikumari-link/MikumariRx.vhd"]"\
@@ -286,10 +284,6 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/hdl/strtdc/lrtdc-impl/defStrLRTDC.vhd"]"\
  "[file normalize "$origin_dir/hdl/strtdc/lrtdc-impl/strLrTdc.vhd"]"\
  "[file normalize "$origin_dir/hdl/toplevel.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/main/defLEDModule.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/main/defMznInterface.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/sitcp/defSiTCP_XG.vhd"]"\
- "[file normalize "$origin_dir/hdl/utility/mikumari/defCDD.vhd"]"\
  "[file normalize "$origin_dir/constrs/pins.xdc"]"\
  "[file normalize "$origin_dir/constrs/timing.xdc"]"\
  "[file normalize "$origin_dir/constrs/gig_ethernet.xdc"]"\
@@ -461,8 +455,7 @@ set files [list \
  [file normalize "${origin_dir}/hdl/mikumari/cbt/CdcmRx.vhd"] \
  [file normalize "${origin_dir}/hdl/mikumari/cbt/CbtRx.vhd"] \
  [file normalize "${origin_dir}/hdl/mikumari/cbt/CbtLane.vhd"] \
- [file normalize "${origin_dir}/hdl/common/main/DCR_NetAssign.vhd"] \
- [file normalize "${origin_dir}/hdl/common/main/DelayGen.vhd"] \
+  [file normalize "${origin_dir}/hdl/common/main/DelayGen.vhd"] \
  [file normalize "${origin_dir}/hdl/strtdc/defDataBusAbst.vhd"] \
  [file normalize "${origin_dir}/hdl/laccp/laccp/defLaccp.vhd"] \
  [file normalize "${origin_dir}/hdl/laccp/laccp/defHeartBeatUnit.vhd"] \
@@ -497,8 +490,7 @@ set files [list \
  [file normalize "${origin_dir}/hdl/laccp/laccp/RLIGP.vhd"] \
  [file normalize "${origin_dir}/hdl/laccp/laccp/RCAP.vhd"] \
  [file normalize "${origin_dir}/hdl/laccp/laccp/LaccpMainBlock.vhd"] \
- [file normalize "${origin_dir}/hdl/common/sitcp/defMiiRstTimer.vhd"] \
- [file normalize "${origin_dir}/hdl/common/sitcp/MiiRstTimer.vhd"] \
+ [file normalize "${origin_dir}/hdl/common/main/RstDelayTimer.vhd"] \
  [file normalize "${origin_dir}/hdl/mikumari/mikumari-link/PRBS16.vhd"] \
  [file normalize "${origin_dir}/hdl/mikumari/mikumari-link/MikumariTx.vhd"] \
  [file normalize "${origin_dir}/hdl/mikumari/mikumari-link/MikumariRx.vhd"] \
@@ -540,10 +532,6 @@ set files [list \
  [file normalize "${origin_dir}/hdl/strtdc/lrtdc-impl/defStrLRTDC.vhd"] \
  [file normalize "${origin_dir}/hdl/strtdc/lrtdc-impl/strLrTdc.vhd"] \
  [file normalize "${origin_dir}/hdl/toplevel.vhd"] \
- [file normalize "${origin_dir}/hdl/common/main/defLEDModule.vhd"] \
- [file normalize "${origin_dir}/hdl/common/main/defMznInterface.vhd"] \
- [file normalize "${origin_dir}/hdl/common/sitcp/defSiTCP_XG.vhd"] \
- [file normalize "${origin_dir}/hdl/utility/mikumari/defCDD.vhd"] \
  [file normalize "${origin_dir}/hdl/common/main/JKFF.vhd"] \
  [file normalize "${origin_dir}/hdl/cirasame-impl/ad9220/defAD9220.vhd"] \
  [file normalize "${origin_dir}/hdl/cirasame-impl/ad9220/AD9220.vhd"] \
@@ -895,12 +883,6 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 set_property -name "library" -value "mylib" -objects $file_obj
 
-set file "$origin_dir/hdl/common/main/DCR_NetAssign.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
-set_property -name "library" -value "mylib" -objects $file_obj
-
 set file "$origin_dir/hdl/common/main/DelayGen.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
@@ -1111,13 +1093,7 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 set_property -name "library" -value "mylib" -objects $file_obj
 
-set file "$origin_dir/hdl/common/sitcp/defMiiRstTimer.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
-set_property -name "library" -value "mylib" -objects $file_obj
-
-set file "$origin_dir/hdl/common/sitcp/MiiRstTimer.vhd"
+set file "$origin_dir/hdl/common/main/RstDelayTimer.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
@@ -1367,30 +1343,6 @@ set file "$origin_dir/hdl/toplevel.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL 2008" -objects $file_obj
-set_property -name "library" -value "mylib" -objects $file_obj
-
-set file "$origin_dir/hdl/common/main/defLEDModule.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
-set_property -name "library" -value "mylib" -objects $file_obj
-
-set file "$origin_dir/hdl/common/main/defMznInterface.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
-set_property -name "library" -value "mylib" -objects $file_obj
-
-set file "$origin_dir/hdl/common/sitcp/defSiTCP_XG.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
-set_property -name "library" -value "mylib" -objects $file_obj
-
-set file "$origin_dir/hdl/utility/mikumari/defCDD.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
 set_property -name "library" -value "mylib" -objects $file_obj
 
 set file "$origin_dir/hdl/common/main/JKFF.vhd"
